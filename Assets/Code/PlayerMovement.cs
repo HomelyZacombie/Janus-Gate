46ff5cdd219c0f6fedc5ced6b26dbb509f1988ad
HomelyZacombie <91287052+HomelyZacombie@users.noreply.github.com>
2024-11-06 04:08:03 +0000

    Combat
    Took a while to find the error in registration of the code but got a combat combo working for the main character

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -11,10 +11,24 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 turn;
     public float mouseSensitivity = 1.6f;
     private Vector3 PlayerMove;
-    public static float speed = 4f;
+    [SerializeField] float SpeedUp = 4f;
+
+    
+
 
     //Aimation controls
     private Animator PlayerAni;
+    private bool WalkOn;
+
+    //combat combo
+    public int comboCounter;
+    float cooldownTime = 0.1f;
+    float lastClicked;
+    float lastcomboEnd;
+    public int comboLength;
+    [SerializeField] float damage;
+    
+
 
     void Start()
     {
@@ -23,24 +37,42 @@ public class PlayerMovement : MonoBehaviour
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        //PlayerAttBox.SetActive(false);
+        WalkOn = false;
     }
 
+    //Player Animations
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
             PlayerAni.SetBool("IsWalking", true);
-            
+            WalkOn = true;
+            //SpeedUp = 4f;
         }
-        if (Input.GetKeyUp(KeyCode.W)) {PlayerAni.SetBool("IsWalking", false);}
+        if (Input.GetKeyUp(KeyCode.W)) { PlayerAni.SetBool("IsWalking", false); WalkOn = false; }
+        //if (Input.GetKeyUp(KeyCode.W)) {PlayerAni.SetBool("IsWalking", false); WalkOn = false; }
 
-        if (Input.GetKey(KeyCode.Mouse0))
+        if (WalkOn == true && (Input.GetKeyDown(KeyCode.LeftShift)))
+        {
+            PlayerAni.SetBool("IsRunning", true);
+            SpeedUp = 8f;
+        }
+        if (WalkOn == false || Input.GetKeyUp(KeyCode.LeftShift)) { PlayerAni.SetBool("IsRunning", false); SpeedUp = 4f; }
+
+
+        /*[SerializeField] void Attacking(){
+            SpeedUp = 0;
+        }*/
+
+        /*if (Input.GetKey(KeyCode.Mouse0))
         {
             PlayerAni.SetBool("Attack", true);
             
             StartCoroutine(Attack(0.6f));
-            speed = 0f;
-
+            WalkSpeed = 0f;
+            
         }
 
         IEnumerator Attack(float waitTime)
@@ -48,8 +80,64 @@ public class PlayerMovement : MonoBehaviour
             yield return new WaitForSeconds(waitTime);
 
             PlayerAni.SetBool("Attack", false);
-            speed = 4f;
 
+        }*/
+        //Attacking animation control
+        if (Input.GetKeyDown(KeyCode.Mouse0) && Time.time - lastcomboEnd > cooldownTime)
+        {
+            
+            comboCounter++;
+            comboCounter = Mathf.Clamp(comboCounter, 0, comboLength);
+            //Attacking();
+
+            for(int i = 0; i < comboCounter; i++)
+            {
+                if(i == 0)
+                {
+                    if(comboCounter == 1 && PlayerAni.GetCurrentAnimatorStateInfo(0).IsTag("Movement"))
+                    {
+                        PlayerAni.SetBool("StartAttack", true);
+                        PlayerAni.SetBool("Attack" + (i + 1), true);
+                        lastClicked = Time.time;
+                        SpeedUp = 0f;
+                    }
+                }
+                else
+                {
+                    
+                    if (comboCounter >= (i+1) && PlayerAni.GetCurrentAnimatorStateInfo(0).IsName("Attack" + i))//IsName("Attack" +i)
+                    {
+                       
+                        PlayerAni.SetBool("Attack" + (i + 1), true);
+                        PlayerAni.SetBool("Attack" + (i), true);
+                        lastClicked = Time.time;
+                        SpeedUp = 0f;
+
+                    }
+                }
+            }
+        }
+
+        //animation exit bool reset
+
+        for(int i=0; i < comboLength; i++)
+        {
+          
+            if (PlayerAni.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.9f && PlayerAni.GetCurrentAnimatorStateInfo(0).IsName("Attack" + (i + 1)))
+            {
+               
+                comboCounter = 0;
+                lastcomboEnd = Time.time;
+                PlayerAni.SetBool("Attack" + (i + 1), false);
+                PlayerAni.SetBool("StartAttack", false);
+                SpeedUp = 4f;
+
+                if (WalkOn == true && (Input.GetKeyDown(KeyCode.LeftShift)))
+                {
+                    PlayerAni.SetBool("IsRunning", true);
+                    SpeedUp = 8f;
+                }
+            }
         }
     }
 
@@ -70,7 +158,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void MovePlayer()
     {
-        Vector3 MoveVector = transform.TransformDirection(PlayerMove) * speed;
+        Vector3 MoveVector = transform.TransformDirection(PlayerMove) * SpeedUp;
         PlayerBody.velocity = new Vector3(MoveVector.x, PlayerBody.velocity.y, MoveVector.z);
 
     }
